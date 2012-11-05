eed69819e0bb2c7ffdee3a147a06fa9e67e4cd2b
hbddingding <hebiduhebi@126.com>
2012-11-05 17:34:46 +0800

    为AssetModel增添了资源描述解析功能
    Signed-off-by: hbddingding <hebiduhebi@126.com>

--- framework/models/AssetModel.js
+++ framework/models/AssetModel.js
@@ -6,6 +6,7 @@
  //@trigger res_desc_ready事件 
  //@trigger loadedAsset事件 
  //@trigger start事件 ，触发游戏模型进行循环
+ var getLoop = 0;
 var AssetModel = {
 	isReadyToLoad:false,//是否准备好载入资源(图片，音乐)
 	mapsLoaded:false,
@@ -18,30 +19,111 @@ var AssetModel = {
             Log("argument of data is undefined!");
             return;
         }
-		this.jsonRes = data;
+		if(this.isReadyToLoad )
+		{
+			Log("已经准备好，无需再解析");
+			return ;
+		}
+		this.jsonRes = (data);
        // jsonRes = JSON.stringify(data);
-        Log("数据转为JSON后: "+JSON.stringify(this.jsonRes));
+//        Log("数据转为JSON后: "+JSON.stringify(this.jsonRes));
 		this.isReadyToLoad = true;
+		Log(this.jsonRes);
+		for(var i=this.jsonRes .length-1;i>=0;i--)
+		{
+			var objtmp =  new Object();
+			var j;
+			var tmp = this.jsonRes[i]["res"]["bgs"];
+			Log("背景资源数目:"+tmp.length);
+			objtmp["bgs"] = [];
+			objtmp["bgs"].push(0);//标记已载入资源数目
+			for( j=tmp.length-1;j>=0;j--)
+			{
+				console.log(tmp[j]);
+				objtmp["bgs"].push(tmp[j]);
+			}
+			tmp = this.jsonRes[i]["res"]["roles"];
+			Log("角色资源数目:"+tmp.length);			
+			objtmp["roles"] = [];
+			objtmp["roles"].push(0);//标记已载入资源数目
+			for( j=tmp.length-1;j>=0;j--)
+			{
+				console.log(tmp[j]);
+				objtmp["roles"].push(tmp[j]);
+				
+			}
+			tmp = this.jsonRes[i]["res"]["skills"];
+			Log("技能资源数目:"+tmp.length);	
+			objtmp["skills"] = [];
+			objtmp["skills"].push(0);//标记已载入资源数目
+			for( j=tmp.length-1;j>=0;j--)
+			{
+				console.log(tmp[j]);
+				objtmp["skills"].push(tmp[j]);
+			}
+			/*
+			tmp = this.jsonRes[i]["res"]["musics"];
+			Log("音乐资源数目:"+tmp.length);
+			objtmp["musics"] = [];
+			objtmp["musics"].push(0);//标记已载入资源数目
+			for( j=tmp.length-1;j>=0;j--)
+			{
+				objtmp["musics"].push(tmp[j]);			
+			}*/
+			this.resLevel.push(objtmp);
+			
+		}
+		console.log("资源解析后的结果: ");
+		console.log(this.resLevel);
+
+
+
+
+
+
 		MFGEvent.fireEvent(mfgEvents.res_desc_ready);//触发ready事件
+		
     },
     //获得资源描述字符串
     getResDesc: function(resWhich) {
 		if(this.isReadyToLoad) return ;//已经准备好了直接返回，
-		var resdescUrl= getResUrl(resWhich,mfgConfig.bLoadFromLocal);
+		
+		var resdesc = window.localStorage.getItem("MFG_RES_DESC");
+		if(resdesc && typeof resdesc !="undefined"){
+			//Log(resdesc.toArray());
+			this.parseResDesc(eval(resdesc));
+			Log("从缓存成功获取资源描述。");
+			return ;
+		}
+		var resdescUrl= getResUrl(resWhich,mfgConfig.bLoadFromLocal);					
+		
         var anThis = this;
         $.ajax({
 			url:resdescUrl,
 			type:"GET",
-			timeout:4000,
+			timeout:6000,
 			dataType:"script",
 			context:anThis,
-			success: function(){		
-				Log("成功获取资源描述。");
-				anThis.parseResDesc(MFG_RES_DESC);	
+			success: function(){
+				if(mfgConfig.bLoadFromLocal)
+				{
+					Log("从本地，成功获取资源描述。");
+				}else{
+					Log("从远程，成功获取资源描述。");
+				}
+				anThis.parseResDesc(MFG_RES_DESC);
+				if(typeof window.localStorage != 'undefined'){
+				window.localStorage.setItem("MFG_RES_DESC",JSON.stringify(MFG_RES_DESC));
+				Log("设置缓存 MFG_RES_DESC");
+				}
 			},
 			error:function(){
 				Log("无法从远程获取，将从本地获取！");
-				mfgConfig.bLoadFromLocal = true;
+				if(getLoop > 4) {
+					Log("从本地，远程，缓存都无法获取到数据。");
+				}
+				mfgConfig.bLoadFromLocal = !mfgConfig.bLoadFromLocal;
+				getLoop ++;
 				anThis.getResDesc(resWhich);
 			}
         });
@@ -84,6 +166,8 @@ var AssetModel = {
     isOnline: function() {
         return navigator.onLine;
     },
+	resLevel : [
+	],
     //资源
     resources: {
 	bgs:[],
@@ -91,6 +175,17 @@ var AssetModel = {
 	roles:[],
 	skills:[],
 	musics:[]
+	},
+	bindImageEventHandler:function(img){
+		img.onerror = function(){ console.log("img.onerror")};
+		img.onabort = function(){
+		console.log(img.obabort);};
+		img.onload = function(){
+			AssetModel.mapsLoaded = true;
+			Log("image onload one",mfgConfig.toUserLevel);
+			MFGEvent.fireEvent(mfgEvents.loadedAsset);
+			Log("fireEvent loadedAsset");
+		};
 	}
 };
 //​​
